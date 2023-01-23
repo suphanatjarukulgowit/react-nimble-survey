31533bcedf5b9385c02285a75acb5b6f11b7dedc
Suphanat Jarukulgowit <suphanat@nimblehq.co>
2023-01-23 19:40:08 +0700

    [#5] Add layout auth template and login screen

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,16 +2,11 @@ import React from 'react';
 
 interface ButtonProps {
   name: string;
-  disabled?: boolean;
   className?: string;
 }
 
-const Button = ({ name, disabled, className }: ButtonProps): JSX.Element => {
-  return (
-    <button disabled={disabled} className={className}>
-      {name}
-    </button>
-  );
+const Button = ({ name, className }: ButtonProps): JSX.Element => {
+  return <button className={className}>{name}</button>;
 };
 
 export default Button;
