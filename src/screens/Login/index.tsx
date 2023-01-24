9ca20095ee439e896bbe3bc3ff06b6cc77e4f7b9
Suphanat Jarukulgowit <suphanat@nimblehq.co>
2023-01-24 10:49:30 +0700

    [#5] Add alert component

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,34 +1,38 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { isEmpty } from 'lodash';
 import validator from 'validator';
 
+import Alert from 'components/Alert';
 import Button from 'components/Button';
+import WarningIcon from 'components/Icon/WarningIcon';
 import Input from 'components/Input';
 import AuthLayout from 'components/Layout/Auth';
 
 const LoginScreen = (): JSX.Element => {
   const { t } = useTranslation();
   const [formInput, setformInput] = useState({ email: '', password: '' });
+  const [errorMessage, setErrorMessage] = useState<string[]>([]);
+  const [formLoading, setFormLoading] = useState(false);
   const validateInput = (input: any) => {
-    if (!(input.email && input.password)) {
-      return 'email and password must not blank';
+    const errors = [];
+    if (!input.email) {
+      errors.push(t('error.email_blank'));
     }
-    if (validator.isEmail(input.email)) {
-      return '';
-    } else {
-      return 'invalid email';
+    if (!input.password) {
+      errors.push(t('error.password_blank'));
     }
+    if (!validator.isEmail(input.email)) {
+      errors.push(t('error.email_invalid'));
+    }
+    setErrorMessage(errors);
   };
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const errMessage = validateInput(formInput);
-    if (errMessage === '') {
-      console.log('proceed login');
-    } else {
-      // pop up error
-      alert(errMessage);
-    }
+    setFormLoading(true);
+    validateInput(formInput);
+    setFormLoading(false);
   };
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setformInput({ ...formInput, [event.target.name]: event.target.value });
@@ -37,10 +41,11 @@ const LoginScreen = (): JSX.Element => {
   return (
     <AuthLayout headerMessage={t('auth.heading')}>
       <div className="form-group">
+        {errorMessage && !isEmpty(errorMessage) && <Alert Icon={WarningIcon} errorMessage={errorMessage}></Alert>}
         <form onSubmit={handleSubmit}>
           <Input label={t('auth.sign_in')} onChange={handleChange} type="text" name="email"></Input>
           <Input label={t('auth.password')} onChange={handleChange} type="password" name="password"></Input>
-          <Button name={t('auth.sign_in')} className="sign-in-btn"></Button>
+          <Button disabled={formLoading} name={t('auth.sign_in')} className="sign-in-btn"></Button>
         </form>
       </div>
     </AuthLayout>
