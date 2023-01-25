9e8e1504b10e9d2b623e3a0adc33bd9ba6246675
Suphanat Jarukulgowit <suphanat@nimblehq.co>
2023-01-25 20:38:46 +0700

    [#6] Add interceptors for request's header and refresh, log out api

--- src/adapters/Auth/index.ts
+++ src/adapters/Auth/index.ts
@@ -11,6 +11,23 @@ const AuthAdapter = {
     };
     return requestManager('POST', '/api/v1/oauth/token', { data });
   },
+  refresh: (refreshToken: string) => {
+    const data = {
+      grantType: 'refresh_token',
+      refreshToken: refreshToken,
+      clientId: process.env.REACT_APP_API_CLIENT_ID,
+      clientSecret: process.env.REACT_APP_API_CLIENT_SECRET,
+    };
+    return requestManager('POST', '/api/v1/oauth/token', { data });
+  },
+  logOut: (token: string) => {
+    const data = {
+      token: token,
+      clientId: process.env.REACT_APP_API_CLIENT_ID,
+      clientSecret: process.env.REACT_APP_API_CLIENT_SECRET,
+    };
+    return requestManager('POST', '/api/v1/oauth/revoke', { data });
+  },
 };
 
 export default AuthAdapter;
