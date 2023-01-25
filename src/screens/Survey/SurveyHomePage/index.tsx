811b594e2c19ea4bcaaae4513eec1045883d8362
Suphanat Jarukulgowit <suphanat@nimblehq.co>
2023-01-25 20:38:46 +0700

    [#6] Add interceptors for request's header and refresh, log out api

--- src/screens/Survey/SurveyHomePage/index.tsx
+++ src/screens/Survey/SurveyHomePage/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import AuthAdapter from 'adapters/Auth';
+import useAuth from 'hooks/useAuth';
+import ApiError from 'lib/errors/ApiErrors';
 
 const SurveyHomepageScreen = (): JSX.Element => {
-  return <div className="app">This is Home Screen!</div>;
+  const { auth, setAuth } = useAuth();
+  const navigate = useNavigate();
+  const testLogOut = () => {
+    if (auth) {
+      AuthAdapter.logOut(auth.accessToken)
+        .then(() => {
+          setAuth(null);
+          navigate('/');
+        })
+        .catch((error) => {
+          if (error instanceof ApiError) {
+            console.log('Api error', error);
+          } else {
+            console.log('else error : ', error);
+          }
+        });
+    }
+  };
+  return (
+    <div className="app">
+      This is Home Screen!
+      <button onClick={testLogOut}>Logout</button>
+    </div>
+  );
 };
 
 export default SurveyHomepageScreen;
