2829a1139b13cfcdd5397849121dac8ca5be67d7
Suphanat Jarukulgowit <suphanat@nimblehq.co>
2023-02-01 14:28:42 +0700

    [#5] Delete unused UI test

--- cypress/integration/home.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('home page', () => {
-  it('the p tag contains the correct text', () => {
-    cy.visit('http://localhost:3000');
-    cy.get('p').should('exist').contains('Edit <code>src/App.tsx</code> and save to reload');
-  });
-});
