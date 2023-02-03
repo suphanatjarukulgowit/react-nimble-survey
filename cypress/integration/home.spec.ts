b8d80d602eef1dfb43b1c60b73b3ae534ca07ac4
Suphanat Jarukulgowit <suphanat@nimblehq.co>
2023-02-03 09:35:04 +0700

    [#3] Update data test id in home.spec.ts for UI test

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -2,6 +2,6 @@ describe('home page', () => {
   it('displays the correct text', () => {
     cy.visit('/');
 
-    cy.findByTestId('home-message').contains('Edit <code>src/App.tsx</code> and save to reload');
+    cy.findByTestId('homeMessage').contains('Edit <code>src/App.tsx</code> and save to reload');
   });
 });
