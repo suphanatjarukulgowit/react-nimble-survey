4afbade1c83d846202e3c92596d78b537708df64
Suphanat Jarukulgowit <suphanat@nimblehq.co>
2023-02-04 22:15:32 +0700

    [#5] Delete template UI test

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,7 +1,5 @@
 describe('home page', () => {
   it('displays the correct text', () => {
     cy.visit('/');
-
-    cy.findByTestId('homeMessage').contains('Edit <code>src/App.tsx</code> and save to reload');
   });
 });
