5d9e578b504d58892d495dbeb41704c48cde6a1f
Suphanat Jarukulgowit <suphanat@nimblehq.co>
2023-02-08 20:04:26 +0700

    [#12] Fix skeleton loader style

--- src/components/SurveyLoding/index.tsx
+++ src/components/SurveyLoding/index.tsx
@@ -8,10 +8,12 @@ const SurveyLoading = () => {
       <SkeletonLoader width={36} height={36} className="user-avatar-loader" />
       <SkeletonLoader width={150} height={25} className="date-loader" />
       <SkeletonLoader width={100} height={25} className="date-today-loader" />
-      <SkeletonLoader width={704} height={302} className="survey-list-loader" />
+      <div className="survey-list-loader-container">
+        <SkeletonLoader width={704} height={302} className="survey-list-loader" />
+        <SkeletonLoader width={56} height={56} className="survey-link-loader" />
+      </div>
       <SkeletonLoader width={318} height={25} className="survey-title-loader" />
       <SkeletonLoader width={240} height={25} className="survey-description-loader" />
-      <SkeletonLoader width={56} height={56} className="survey-link-loader" />
     </div>
   );
 };
